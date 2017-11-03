66b036b0c3d7157ac490e70dced72db6aebe530d
César Casas <cesarcasas@bsdsolutions.com.ar>
2017-11-02 21:54:16 -0300

    routers

--- class1/completeapp/src/app/index.jsx
+++ class1/completeapp/src/app/index.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { createStore } from 'redux';
 import { render } from 'react-dom';
-import TodosList from './containers/TodosList/index.jsx';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import { createStore, applyMiddleware } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+
 import { default as todos } from './reducers';
 import { initialState } from './actions';
 
-const store = createStore(todos);
+const history = createHistory();
 
-const App = ({ state, dispatch, update }) => (
-  <TodosList todos={state} dispatch={dispatch} update={update} />
+const store = createStore(
+    todos,
+    applyMiddleware(routerMiddleware(history)),
 );
 
+const ConnectedSwitch = connect(state => ({
+	location: state.location
+}))(Switch);
 
-const appRender = (store, update)=> render( <App state={store.getState()} dispatch={store.dispatch} update={update} />, document.getElementById('app') );
 
-store.subscribe(() => appRender(store) );
 
-appRender(store);
+const AppContainer = ({ location }) => (
+    <ConnectedSwitch>
+        <Route exact path="/" component={(props) => (<h1>Home <Link to="/about">About</Link></h1>)} />
+        <Route path="/about" component={(props) => (<h1>About <Link to="/">Home</Link></h1>)} />
+    </ConnectedSwitch>
+);
+
+const App = connect(state => ({
+    location: state.location,
+}))(AppContainer);
+
+render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('app'),
+);
