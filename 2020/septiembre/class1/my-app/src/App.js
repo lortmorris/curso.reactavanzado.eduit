fe19e892d38005e47ca7b07fac84ad9444ecae34
Cesar Casas <cesarcasas@bsdsolutions.com.ar>
2020-09-26 11:06:25 -0300

    redux, todos app

--- 2020/septiembre/class1/my-app/src/App.js
+++ 2020/septiembre/class1/my-app/src/App.js
@@ -4,29 +4,19 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+
+import About from './screens/About';
+import Users from './screens/Users';
+import Home from './screens/Home';
+
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        <NavBar />
         <Switch>
           <Route path="/about">
             <About />
@@ -41,18 +31,6 @@ function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
---
+++ 2020/septiembre/class1/my-app/src/reducers/todos.js
@@ -0,0 +1,38 @@
+
+
+const idx = () => new Date().getTime();
+
+const Todo = (initialState = {}, action) => {
+  switch(action.type) {
+    case 'ADD':
+      return {
+        ...action.payload,
+        complete: false,
+        added: new Date(),
+        id: idx(),
+      };
+    case 'TOGGLE':
+      if (action.payload.id === initialState.id) {
+        return {
+          ...initialState,
+          complete: !initialState.complete,
+        };
+      }
+      return initialState;
+    default:
+      return initialState;
+  }
+}
+
+const Todos = (initialState = [], action) => {
+    switch(action.type) {
+      case 'ADD':
+        return [...initialState, Todo(action)];
+      case 'TOGGLE':
+        return initialState.map(state => Todo(state, action));
+      default:
+        return initialState;
+    }
+};
+
+export default Todos;
